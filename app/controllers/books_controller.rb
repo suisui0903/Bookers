948847b8e815e5ad25d9db7c8fa1ba8c7e8f4af9
川上鈴乃 <kawakamisuzuno@kawakamisuzunonoMacBook-Pro.local>
2020-02-13 14:46:00 +0900

    [finish]4-8 課題

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def new
+  	@book = Book.new
+  end
+  def create
+        # ストロングパラメーターを使用
+         @book = Book.new(book_params)
+        # DBへ保存する
+        if @book.save
+        redirect_to book_path(@book.id),notice: "Book was successfully created."
+        else
+        	@books = Book.all
+         render 'index'
+    	end
+  	end
+
+    def index
+    	@books = Book.all
+    	@book = Book.new
+    end
+
+    def show
+         @book = Book.find(params[:id])
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        redirect_to book_path(book.id),notice: "Book was successfully update."
+    end
+
+    def destroy
+    	book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path,notice: "Book was successfully destroyed."
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+end
